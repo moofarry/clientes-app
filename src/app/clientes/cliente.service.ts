b3c79ec03e30b2de4f1187ffef74cf570fb75bba
moofarry <jhon.moofarry00@usc.edu.co>
2021-05-20 11:30:33 -0500

    service create cliente

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,34 +2,30 @@ import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
 import { Observable, of } from 'rxjs';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-
-
 
 @Injectable({
   providedIn: 'root',
 })
-
 export class ClienteService {
-  private urlEndPoint: string = "http://localhost:8080/api/clientes";
+  private urlEndPoint: string = 'http://localhost:8080/api/clientes';
+  private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
 
-  constructor(private http: HttpClient) {} 
+  constructor(private http: HttpClient) {}
 
-  getClientes(): Observable<Cliente[]> { //string
+  getClientes(): Observable<Cliente[]> {
+    //string
     //return of(CLIENTES); //flujo observable  a partir de los objetos clientes
     //return this.http.get<Cliente[]> (this.urlEndPoint) -> sin rxjs
-    return this.http.get(this.urlEndPoint).pipe(
-      map( (response) => response as Cliente[])
-    );
-    
+    return this.http
+      .get(this.urlEndPoint)
+      .pipe(map((response) => response as Cliente[]));
+  }
 
-    // create(cliente: Cliente) : Observable<Cliente>{
-    //   map( (response) => response as Cliente[])
-
-    // }
-
-
-    
+  create(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, {
+      headers: this.httpHeaders,
+    });
   }
 }
--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
 
 
 @Component({
@@ -11,14 +13,16 @@ export class FormComponent implements OnInit {
   public titulo: string = "Crear Cliente";  //---> private?
   public cliente: Cliente = new Cliente;
   
-  constructor() { }
+  constructor( private clienteService: ClienteService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
   public create(): void{
-    console.log("Clicked!");
-    console.log(this.cliente);
+    this.clienteService.create(this.cliente).subscribe(
+      response => this.router.navigate(['/clientes'])
+    )
   }
 
 }
